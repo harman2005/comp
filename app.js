4b2c4659baf938484569f88a1da6d536c5894379
Harmanpal Singh <c0757701@mylambton.ca>
2020-03-10 16:59:30 -0400

    Added csv reader

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const app = express()
+const csv = require('csv-parser');
+const fs = require('fs');
+
+ 
  
 app.get('/', function (req, res) {
-function x(){
-console.log('hi');
-}
-	setInterval(x,2000);
+
+fs.createReadStream('Dataset.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    console.log(row);
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed');
+  });
+
   res.send('Hello World')
 })
+
+
+
  
 app.listen(3000)
 
